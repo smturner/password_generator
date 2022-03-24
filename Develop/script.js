47f3a9279a592380af3120d2423f6f83fb5c05ff
Sarah Turner <turnersarah228@gmail.com>
2022-03-24 18:18:20 -0500

    added pseudocode document and commented script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+//variables to use in the code
 var generateBtn = document.querySelector("#generate");
 var numberChars = [0,1, 2, 3, 4, 5,6, 7, 8,9];
 var upperChars = ["A", "B", "C", "D"," E"," F", "G", "H", "I", "J" ,"K" ,"L" ,"M" ,"N", "O", "P", "Q", "R", "S", "T" ,"U", "V" ,"W", "X", "Y", "Z"];
@@ -13,27 +13,25 @@ var lengthInput
 
 
 
-
+//generatePassword function runs the entire password generator
 function generatePassword() {
-    // generateBtn.onclick=generatePassword;
 lengthInput = prompt("Choose a number between 8 and 128");
 var lengthInput = parseInt(lengthInput)
 console.log (lengthInput)
 if (lengthInput < 8 || lengthInput > 128) {
   alert("Password must be between 8-128 characters");
-
   return null
 }
 
-
+//the confirm buttons to choose password conditions
   lowerConfirm = confirm ("Click OK to including lower case letters in password");
   upperConfirm = confirm ("Click OK to including upper case letters in password");
   numConfirm = confirm ("Click OK to including numbers in password");
   specialConfirm = confirm ("Click OK to including special characters in password");
-  console.log (lowerConfirm);
-  console.log (upperConfirm);
-  console.log (numConfirm);
-  console.log (specialConfirm);
+  // console.log (lowerConfirm);
+  // console.log (upperConfirm);
+  // console.log (numConfirm);
+  // console.log (specialConfirm);
 
 //if none are true
 if(!lowerConfirm && !upperConfirm && !numConfirm && !specialConfirm) {
@@ -89,9 +87,9 @@ else if (lowerConfirm && upperConfirm && numConfirm){
   }else if (specialConfirm){
     selectedChar=specialChars;
   }
-
+//empty array
   var thePassword = []
-
+//for loop that selects the random characters for the passwordd
   for(var i=0; i < lengthInput; i++) {
     var pickChoices = selectedChar[Math.floor(Math.random() * selectedChar.length)]; 
   thePassword.push(pickChoices);
@@ -100,13 +98,13 @@ else if (lowerConfirm && upperConfirm && numConfirm){
 
 }
 
-
-// generatePassword ()
-
+//variable to display the password on the screen
 var pw = thePassword.join("")
 
 return pw
+
 }
+//function that actually writes the password and calls the generatePassword function
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -114,51 +112,10 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Makes the button clickable to start the password questions
 generateBtn.addEventListener("click", writePassword);
 
 
 
- // }  
-
-  //var randomstring = math.random().toString(128).slice(-8);
-//console.log (numbers)
-  //else 
-   //console.log ("error")
 
 
-
-
-// function indexRandomizer () {
-
-// }
-
-//function generatePassword(passwordLength) {
-//return ""
-//}
-
-// Write password to the #password input
-
-
-//1. create variables to save user input from prompts (clicking comfirm (true,) cancel (false))
-    //var prompt and var inputs
-    // var for passwordLength- string figure out how to change into an intiger
-      //var specialCharacters == confirm "chick Ok to confirm including special characters" will be a boolean
-      // numericCharacters boolean
-      //lowercaseCharacters boolean
-      //uppercaseCharacters boolean
-    // alert, confirm, prompt
-    // use conditionals to validate inputs (if else) (password length,, user enters no imputs)
-    //When user says no if else?
-//2. create an array with special characters, uppercase letters, lowercase letters, numbers
-  //specialCharacters, Uppercase, lowercase, numeric
-  //3. use randomizer function to select characters from each array
-//4. generatePassword function --take user input, grab charcters from array and generate password
-
-//5. create an empty array that represents the final password
-
-//6. join the character arrays together based on user input
-  // randomize characters from the joined array
-
-  //transforming the array into a string-you can use join
-  //array.join ('') 
